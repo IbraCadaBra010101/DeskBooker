c0d0ede3af16de6f208dd052202eebfd68b1fb62
46790 <46790@LAPTOP-M22C1HP9>
2021-05-26 14:52:11 +0200

    ShouldCallBookDeskMethodOfProcessor

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -1,10 +1,37 @@
-﻿using NUnit.Framework;
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Moq;
+using NUnit.Framework;
 
 namespace DeskBooker.Web.Pages
 {
-  [TestFixture]
-  public class BookDeskModelTests
-  {
+    [TestFixture]
+    public class BookDeskModelTests
+    {
+        //[SetUp]
 
-  }
+        //public void Setup()
+        //{
+
+        //}
+
+        [Test]
+        public void ShouldCallBookDeskMethodOfProcessor()
+        {
+            // Arrange
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+            // Act
+            bookDeskModel.OnPost();
+
+            // Assert
+            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), Times.Once);
+
+
+
+        }
+    }
 }
--- DeskBooker.Web/Pages/BookDesk.cshtml.cs
+++ DeskBooker.Web/Pages/BookDesk.cshtml.cs
@@ -1,17 +1,25 @@
 ﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DeskBooker.Web.Pages
 {
-  public class BookDeskModel : PageModel
-  {
-    [BindProperty]
-    public DeskBookingRequest DeskBookingRequest { get; set; }
-
-    public void OnPost()
+    public class BookDeskModel : PageModel
     {
-      
+        private IDeskBookingRequestProcessor _bookingRequestProcessor;
+
+        public BookDeskModel(IDeskBookingRequestProcessor bookingRequestProcessor)
+        {
+            _bookingRequestProcessor = bookingRequestProcessor;
+        }
+
+        [BindProperty]
+        public DeskBookingRequest DeskBookingRequest { get; set; }
+
+        public void OnPost()
+        {
+            _bookingRequestProcessor.BookDesk(DeskBookingRequest);
+        }
     }
-  }
 }
